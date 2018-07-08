c05d2e578982ffe6248c30be38125b13e648eaa3
Lewis_A <lewisalderman95@gmail.com>
2018-07-08 18:47:45 +0100

    Layout changes

--- fair-teams/src/components/Form.jsx
+++ fair-teams/src/components/Form.jsx
@@ -10,27 +10,29 @@ const Form = ({
 	btnValue,
 }) => {
 	return (
-		<form onSubmit={handleSubmit}>
-			<label htmlFor="name">Name</label>
-			<br />
-			<input
-				id="name"
-				name="player_name"
-				type="text"
-				placeholder="Enter player name"
-				onChange={handleInput}
-				value={input}
-				autoComplete="off"
-			/>
-			<ReactStars
-				onChange={handleRating}
-				className="stars"
-				count={5}
-				value={rating}
-				half={false}
-			/>
-			<input type="submit" value={btnValue} />
-		</form>
+		<div className="form-container">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">Player Name</label>
+				<br />
+				<input
+					id="name"
+					name="player_name"
+					type="text"
+					placeholder="Enter a player name"
+					onChange={handleInput}
+					value={input}
+					autoComplete="off"
+				/>
+				<ReactStars
+					onChange={handleRating}
+					className="stars"
+					count={5}
+					value={rating}
+					half={false}
+				/>
+				<input type="submit" value={btnValue} />
+			</form>
+		</div>
 	);
 };
 
--- fair-teams/src/components/List.jsx
+++ fair-teams/src/components/List.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const List = ({ players, handleRemove, handleEdit }) => {
 	return (
-		<ul>
-			{[].concat(...Object.values(players)).map((player, i) => (
-				<li data-key={i} key={i}>
-					{player.name} :: {player.rating}&#x2605;
-					<button onClick={handleRemove}>X</button>
-					<button onClick={handleEdit}>edit</button>
-				</li>
-			))}
-		</ul>
+		<div className="list-container">
+			<ul>
+				{[].concat(...Object.values(players)).map((player, i) => (
+					<li data-key={i} key={i}>
+						{player.name} :: {player.rating}&#x2605;
+						<button onClick={handleRemove}>X</button>
+						<button onClick={handleEdit}>edit</button>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 };
 
