f3f1748c59448aa0e989e3a681bfc90710575892
Lewis_A <lewisalderman95@gmail.com>
2018-07-11 15:24:26 +0100

    Changed SVG attributes to correct camelCase

--- fair-teams/src/components/List.jsx
+++ fair-teams/src/components/List.jsx
@@ -28,10 +28,10 @@ const List = ({ players, handleRemove, handleEdit }) => {
 											viewBox="0 0 24 24"
 											fill="none"
 											stroke="currentColor"
-											stroke-width="2"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											class="feather feather-edit-2"
+											strokeWidth="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											className="feather feather-edit-2"
 										>
 											<polygon points="16 3 21 8 8 21 3 21 3 16 16 3" />
 										</svg>
