926dfb1e5fb4dea1dd5c3ffe5c8fb4a1e8204cb3
Lewis_A <lewisalderman95@gmail.com>
2018-07-11 14:38:15 +0100

    Changed action buttons to use icons instead of text

--- fair-teams/src/components/List.jsx
+++ fair-teams/src/components/List.jsx
@@ -20,13 +20,46 @@ const List = ({ players, handleRemove, handleEdit }) => {
 									onClick={handleEdit}
 									className="btn btn-icon btn-primary"
 								>
-									Edit
+									<div className="icon icon-edit">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											class="feather feather-edit-2"
+										>
+											<polygon points="16 3 21 8 8 21 3 21 3 16 16 3" />
+										</svg>
+									</div>
 								</button>
 								<button
 									onClick={handleRemove}
 									className="btn btn-icon btn-danger"
 								>
-									Remove
+									<div className="icon icon-remove">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											stroke-width="2"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											class="feather feather-trash-2"
+										>
+											<polyline points="3 6 5 6 21 6" />
+											<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+											<line x1="10" y1="11" x2="10" y2="17" />
+											<line x1="14" y1="11" x2="14" y2="17" />
+										</svg>
+									</div>
 								</button>
 							</div>
 						</li>
